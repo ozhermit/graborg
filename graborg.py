f4a0b955fa121eedeaa0fa29eb3bceff0c7a862a
James Lamb <jlamb@atlassian.com>
2015-04-13 20:26:50 +1000

    updated parse_args, fixed the formatting of the entry variable to by more pythonic

--- graborg.py
+++ graborg.py
@@ -5,12 +5,22 @@ time allowing for files with the same name etc.
 '''
 
 import os
-import sys
 import hashlib
 from datetime import datetime
 import shutil
 import urllib2
+import argparse
 
+
+def parse_args():
+    '''
+    parse the in-line arguments.
+    '''
+    parser = argparse.ArgumentParser(description="garborg: file analysis tool")
+    parser.add_argument("-u", "--url", required=True, type=str)
+    args = parser.parse_args()
+
+    return args
 
 
 def grab(url, filename):
@@ -22,22 +32,26 @@ def grab(url, filename):
     AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.137 Safari/537.36')]
     file = opener.open('%s' % url)
     filed = file.read()
-    f = open("samples/%s" %filename, 'wb')
+    f = open("samples/%s" % filename, 'wb')
     f.write(filed)
     f.close()
+
 
 def org(NAME, sample):
     '''
     Md5 downloaded file, write details to log file and rename ot md5 hash.
     '''
-    md5 = hashlib.md5(open('samples/%s' %NAME).read()).hexdigest()
+    md5 = hashlib.md5(open('samples/%s' % NAME).read()).hexdigest()
     logf = open("graborg-log.csv", "a+")
     fsize = os.stat('samples/%s' % NAME).st_size
-    entry = "%s,%s,%s,%s,%s" % (md5, NAME, fsize, \
-    datetime.now().strftime("%d/%m/%Y %H:%M:%S"), sample)
+    entry = '{0},{1},{2},{3},{4}'.format(
+        md5, NAME, fsize, datetime.now().strftime("%d/%m/%Y %H:%M:%S"),
+        sample
+    )
     logf.write(entry + '\n')
     logf.close()
     shutil.move('samples/%s' % NAME, 'samples/%s' % md5)
+
 
 def reqcheck():
     '''
@@ -51,20 +65,22 @@ def reqcheck():
     if not os.path.isfile('%s' % filelog):
         shutil.copyfile('graborg-log.csv-template', 'graborg-log.csv')
 
+
 def main():
     '''
     main graborg process
     '''
 
-    if len(sys.argv) == 1:
-        print "Please add destination file. Eg. \
-         python graborg.py http://example.com/badfile"
-    sample = sys.argv[1]
+    # validate that the command line arguments are correct
+    args = parse_args()
+
+    # sample becomes a arg namespace
+    sample = args.url
     NAME = sample.split('/')[-1]
     reqcheck()
     grab(sample, NAME)
     org(NAME, sample)
 
+
 if __name__ == "__main__":
     main()
-
